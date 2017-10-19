a57612ee989e3f478a23369e4669f7f706e401b9
Billy Le <lebilly87@gmail.com>
2017-10-19 04:29:17 -0700

    #9 fixed scripts, paths, and modules. remove server.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,52 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+// require plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // use to extract the css from webpack
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // clean dist folder
+const webpack = require('webpack'); // to access built-in webpack plugins
+
+// require node modules
+const path = require('path');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' },
+      {
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'file-loader'],
+        exclude: path.resolve(__dirname, 'src/index.html')
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('index.css'),
     new HtmlWebpackPlugin({
-      title: 'freeCodeCamp Sacramento',
-      filename: './dist/index.html',
-      hash: true
+      template: './src/index.html',
+      title: 'freeCodeCamp Sacramento'
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'common' // Specify the common bundle's name.
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost',
+    open: true,
+    port: 3000
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 });
